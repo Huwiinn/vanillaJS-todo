c7d8798ca3be649405f16481db658842c28ed83c
hwuiinn <gnldls8406@gmail.com>
2024-05-24 01:04:31 +0900

    Feat : TodoList 삭제 및 취소선

--- script.js
+++ script.js
@@ -14,8 +14,37 @@ const addTask = (e) => {
 
     listContainer.appendChild(li);
 
-    todoInput.value = "";
+    let span = document.createElement("span");
+    span.innerHTML = "\u00d7";
+    li.appendChild(span);
   }
+  todoInput.value = "";
+  saveTodo();
 };
 
 myForm.addEventListener("submit", addTask);
+
+listContainer.addEventListener("click", (e) => {
+  // console.log("e : ", e.target.tagName);
+  // console.log("e : ", e.target.classList);
+  // console.log("e : ", e.target.classList.toggle("checked"));
+  // console.log("listContainer.innerHTML : ", listContainer.innerHTML);
+
+  if (e.target.tagName === "LI") {
+    e.target.classList.toggle("checked");
+    saveTodo();
+  } else if (e.target.tagName === "SPAN") {
+    e.target.parentElement.remove();
+    saveTodo();
+  }
+});
+
+const saveTodo = () => {
+  localStorage.setItem("todo", listContainer.innerHTML);
+};
+
+const showTodoList = () => {
+  listContainer.innerHTML = localStorage.getItem("todo");
+};
+
+showTodoList();
