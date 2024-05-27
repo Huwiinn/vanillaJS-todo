07ba78e4d7002529f65a768eca5b6202ff811233
hwuiinn <gnldls8406@gmail.com>
2024-05-27 18:00:53 +0900

    Feat : Github Oauth 적용 및 인증보안 연습

--- script.js
+++ script.js
@@ -26,13 +26,25 @@ async function refreshTodoList(update = false) {
         li.innerHTML = todo.title;
         listContainer.appendChild(li);
 
+        let div = document.createElement("div");
+        div.classList.add("icon_wrap");
+        li.appendChild(div);
+
         let span = document.createElement("span");
         span.classList.add("closeBtn");
         span.innerHTML = "\u00d7";
 
+        let editBtn = document.createElement("span");
+        editBtn.classList.add("todoEdit");
+        editBtn.innerHTML = "&#9999"; // 연필 모양의 유니코드 이스케이프 시퀸스 문자
+        div.appendChild(editBtn);
+
+        // 수정 이벤트 리스너 연결
+        editBtn.addEventListener("click", editTodoHandler);
+
         // 삭제 이벤트 리스너 연결
         span.addEventListener("click", () => removeTodo(todo.id));
-        li.appendChild(span);
+        div.appendChild(span);
       });
     };
 
@@ -114,3 +126,95 @@ listContainer.addEventListener("click", (e) => {
 
   updateTodo(id, classList);
 });
+
+// Todo 수정 이벤트 리스너 설정해야함.
+const editTodoHandler = (e) => {
+  // console.log("e : ", e.target.parentElement.parentElement);
+
+  let editInput = document.createElement("input");
+
+  editInput.classList.add("editInput");
+  editInput.setAttribute("type", "text"); // 지정된 요소의 속성 값을 설정 (name, value)
+
+  if (e.target.parentElement.parentElement) {
+    e.target.parentElement.parentElement.remove();
+  }
+
+  // 수정 버튼을 클릭하면, li 요소는 사라지고 Input 요소가 들어와야 함. 최초 클릭시, 기존에 li에 있던 텍스트 value를 기본값으로 둬야할 것.
+  // 토글 형식으로 한 번 클릭하면 input, 두 번 클릭시 기존으로 돌아가도록.
+
+  listContainer.appendChild(editInput);
+};
+
+// 로그인 시도 함수
+const signInWithGithub = async () => {
+  const { data, error } = await client.auth.signInWithOAuth({
+    provider: "github",
+    options: {
+      redirectTo: "http://127.0.0.1:5500",
+    },
+  });
+
+  console.log("oauth data : ", data);
+};
+
+document.querySelector("#login").addEventListener("click", signInWithGithub);
+
+// 로그인 상태 확인
+const checkLogin = async () => {
+  const authInfo = await client.auth.getSession();
+  const session = authInfo.data.session;
+
+  document.querySelector("#login").style.display = "none";
+  document.querySelector("#logout").style.display = "none";
+
+  console.log("session : ", session);
+
+  if (session === null) {
+    document.querySelector("#login").style.display = "block";
+  } else {
+    document.querySelector("#logout").style.display = "block";
+  }
+};
+
+checkLogin();
+
+// 로그아웃
+const signOut = async () => {
+  const { error } = await client.auth.signOut();
+  checkLogin(); // 로그아웃 후에 상태변화 업데이트
+};
+
+document.querySelector("#logout").addEventListener("click", signOut);
+
+const refreshHistory = async () => {
+  let { data: record, error } = await client.from("page").select("*");
+  let tag = "";
+
+  console.log(record);
+
+  for (let i = 0; i < record.length; i++) {
+    tag += `<div style="margin:20px 0"><h2>${record[i].title}</h2>${record[i].body}
+    <p><input type="button" value="delete" onclick="deleteRecord(${record[i].id})" /></p>
+    <p><input type="button" value="update" onclick="updateRecord(${record[i].id})" /></p>
+</div>`;
+  }
+  document.querySelector("#history").innerHTML = tag;
+};
+
+refreshHistory();
+
+const createPost = async () => {
+  const { data, error } = await client
+    .from("page")
+    .insert([{ title: prompt("title?"), body: prompt("body?") }]);
+
+  refreshHistory();
+};
+
+document.querySelector("#create_btn").addEventListener("click", createPost);
+
+const deleteRecord = async (id) => {
+  const { data, error } = await client.from("page").delete().eq("id", id);
+  refreshHistory();
+};
